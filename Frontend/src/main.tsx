3a7a481a6b5784111e9df623b2de45a66618dc03
Surya Pratap Singh <suryapratapsingh.cse26@jecrc.ac.in>
2025-02-15 17:02:17 +0530

    Adding Frontend

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <Router>
     <Toaster />
     <App />
-  </BrowserRouter>
+  </Router>
 );
