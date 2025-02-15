e941448aae26d4bf641068bf0d54f0dee4e26c3d
Surya Pratap Singh <suryapratapsingh.cse26@jecrc.ac.in>
2025-02-15 16:50:42 +0530

    Adding Frontend

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { ConnectKitButton } from "connectkit";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <div className="w-[100%] bg-gray-800 flex justify-center items-center">
       <nav className="max-w-[1000px] w-[100%]  text-white p-4 flex justify-between items-center shadow-lg ">
-        <h1 className="text-2xl font-bold text-yellow-400 text-center">EVM Bridge</h1>
+        <h1 className="text-2xl font-bold text-yellow-400 text-center cursor-pointer" onClick={()=>{navigate("/")}}>EVM Bridge</h1>
         <button className=" text-black py-2 px-4 rounded-full font-semibold hover:bg-yellow-400 transition">
           <ConnectKitButton />
         </button>
